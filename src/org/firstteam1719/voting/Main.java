841ec3d9589d3f3e215b8d7d53dd7b72e5d0c5ac
Aaron Eline <aeline@parkschool.net>
2016-12-29 11:16:30 -0500

    Tabs/Spaces

--- src/org/firstteam1719/voting/Main.java
+++ src/org/firstteam1719/voting/Main.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		File dir = new File(args[0]);
-		String[] top = IRVCounter.rank(JSONVoteRead.readStrings(dir));
-		System.out.println("Top " + top.length + " Priorities\n=================\n");
-		for(int i = 0; i < top.length; i++){
+        File dir = new File(args[0]);
+        String[] top = IRVCounter.rank(JSONVoteRead.readStrings(dir));
+        System.out.println("Top " + top.length + " Priorities\n=================\n");
+        for(int i = 0; i < top.length; i++){
             System.out.println("" + i + ": " + top[i]);
         }
 
-	}
+    }
 
 }
